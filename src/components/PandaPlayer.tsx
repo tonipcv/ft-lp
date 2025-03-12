1e37369c75f486efa5be1cda535c94b7c80f1b80
tonipcv <xppsalvador@gmail.com>
2025-03-12 11:43:37 +0000

    vamos

--- src/components/PandaPlayer.tsx
+++ src/components/PandaPlayer.tsx
@@ -1,23 +1,55 @@
 'use client';
 
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    pandascripttag: (() => void)[];
+    PandaPlayer: new (videoId: string, options: {
+      onReady: () => void;
+      library_id: string;
+      video_id: string;
+      fetchPriority: string;
+      defaultStyle: boolean;
+    }) => void;
+  }
+}
+
 interface PandaPlayerProps {
   videoId: string;
 }
 
 export function PandaPlayer({ videoId }: PandaPlayerProps) {
+  useEffect(() => {
+    // Adiciona o script do Panda Video se ainda não existir
+    if (!document.querySelector('script[src^="https://player.pandavideo.com.br/api.v2.js"]')) {
+      const script = document.createElement('script');
+      script.src = 'https://player.pandavideo.com.br/api.v2.js?nowprocket';
+      script.async = true;
+      document.head.appendChild(script);
+
+      script.onload = initializePlayer;
+    } else {
+      initializePlayer();
+    }
+
+    function initializePlayer() {
+      window.pandascripttag = window.pandascripttag || [];
+      window.pandascripttag.push(function() {
+        new window.PandaPlayer(videoId, {
+          onReady() {
+            // Player está pronto
+          },
+          library_id: 'vz-7b6cf9e4-8bf',
+          video_id: videoId,
+          fetchPriority: "high",
+          defaultStyle: true
+        });
+      });
+    }
+  }, [videoId]);
+
   return (
-    <iframe
-      src={`https://player-vz-6a82bc71-be8.tv.pandavideo.com.br/embed/?v=${videoId}`}
-      style={{
-        border: 'none',
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        height: '100%',
-        width: '100%'
-      }}
-      allow="accelerometer;gyroscope;autoplay;encrypted-media;picture-in-picture"
-      allowFullScreen
-    />
+    <div id={videoId} className="w-full aspect-video" />
   );
 }
